033f52e8fde13b593621268b30cf2fcbc07e0ef4
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2020-04-17 21:31:54 +0430

    create task list & task

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import a from './src/Screen/Main/MainIndex';
-import A from './src/Screen/Login/LoginIndex';
-import m from './src/Screen/Main/TaskList/TaskList';
 
 import {name as appName} from './app.json';
 
